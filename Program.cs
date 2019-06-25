c9549cf755660862777129d93ba3014c9fdf7c6b
gugu <gustavocorrea2032@gmail.com>
2019-06-25 12:38:31 -0300

    For2

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace For2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    string nome = "";
+        Console.WriteLine ("Entre com um texto: ");
+            nome = Console.ReadLine();
+                for(int i = 0 ; i <= nome.Length-1 ; i++ ){
+                    Console.WriteLine(nome[i]);}
+        }
+    }
+}
